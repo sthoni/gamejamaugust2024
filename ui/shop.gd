dd518d34c22a84df1af59c7d2e78140e3311cd31
Sebastian Thon <sthon@hotmail.de>
2024-08-17 21:21:35 +0200

    Erstes Item kann im Shop gekauft werden.

--- ui/shop.gd
+++ ui/shop.gd
@@ -1,10 +1,15 @@
 extends Control
 
+@export var item_one: Item
+@export var item_two: Item
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	%ItemDisplay.set_item(item_one)
+	%ItemDisplay2.set_item(item_two)
 
+func add_item(node: Node) -> void:
+	%ItemsDisplayGrid.add_child(node)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,3 +18,7 @@ func _process(delta: float) -> void:
 func _on_continue_button_pressed() -> void:
 	%Shop.hide()
 	get_tree().paused = false
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
