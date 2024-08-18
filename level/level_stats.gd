873de33fbe16d2a15029e5dc01f3c4c39a8ad591
Sebastian Thon <sthon@hotmail.de>
2024-08-18 10:11:15 +0200

    Alle Resources erstellt.

---
+++ level/level_stats.gd
@@ -0,0 +1,9 @@
+class_name LevelStats extends Resource
+
+@export_group("Level Attributes")
+@export var station_stats: StationStats
+@export var train_stats: TrainStats
+@export var item_pool: ItemPool
+
+@export_group("Level Visuals")
+@export var background_texture: Texture
