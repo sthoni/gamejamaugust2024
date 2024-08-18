58015021de372ddf0682c44c7cd55e9b6302deff
Armin <armin@arminlanger.com>
2024-08-18 12:03:12 +0200

    changed freight big waggon and all around

--- items/freight_big_waggon.gd
+++
@@ -1,8 +0,0 @@
-class_name FreightBigWaggon extends Item
-
-@export var transport_amount := 80.0
-
-func apply_effects(target: Node) -> void:
-	if target is Train:
-		target.weight += self.weight
-		target.transport_amount += transport_amount
