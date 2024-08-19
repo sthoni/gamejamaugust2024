dd7a39b4a4c1ccb013bf5a65c3606033194921c4
Armin <armin@arminlanger.com>
2024-08-19 23:13:22 +0200

    nw item: steam brake, fixed bug: brake still working after new engine

--- items/item_pool.gd
+++ items/item_pool.gd
@@ -8,7 +8,6 @@ func get_random_item() -> Item:
 	var item: Item = items[randi() % items.size()]
 	return item
 
-
 func get_two_unique_random_items() -> Array[Item]:
 	var item_1: Item = get_random_item()
 	var item_2: Item = get_random_item()
