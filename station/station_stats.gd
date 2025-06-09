935f144ba3da262a8522a5e620a8666f02d7f4fe
Sebastian <sthon@hotmail.de>
2025-06-09 02:29:44 +0200

    Standard Plattform-Länge eingefügt. Die funktioniert zumindest.

--- station/station_stats.gd
+++ station/station_stats.gd
@@ -3,5 +3,5 @@ class_name StationStats extends Resource
 @export_group("Station Attributes")
 @export var name: String
 @export var reward: int
-@export var platform_length: float
+@export var platform_length: float = 80.0
 @export var sprite: Texture
